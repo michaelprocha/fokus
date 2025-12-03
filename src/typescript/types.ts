f321db7d5c6511fb06530968877cdf4f549d4697
michaelprocha <michaelrocha9722@gmail.com>
2025-12-03 17:54:40 -0300

    add interface model, view and controller

--- src/typescript/types.ts
+++ src/typescript/types.ts
@@ -1,11 +1,36 @@
+type Status = true | false;
+
 interface Task {
-    id: number,
+    id: number;
     content: string;
     done: boolean;
 }
 
 type TaskCreate = Omit<Task, "id">;
 
-type Status = true | false;
+interface TaskModel {
+    tasks: Task[];
+    getAllTasks(): Task[] | string;
+    addTask(task: TaskCreate): Task | string;
+    deleteTaks(id: number): string;
+    editTask(id: number, content: string): string;
+}
 
-export type {Task, Status, TaskCreate};
+interface TaskView {
+    renderAllTask(tasks: Task[] | string): void;
+    renderAddTask(task: Task | string): void;
+    renderRemoveTask(message: string): void;
+    renderEditTask(message: string): void;
+}
+
+interface TaskController {
+    model: TaskModel;
+    view: TaskView;
+    setupEventListeners(): void;
+    handleStartView(): void;
+    handleAddTask(): void;
+    handleRemoveTask(id: number): void;
+    handleEditTask(id: number): void;
+}
+
+export type { Status, Task, TaskCreate, TaskModel, TaskView, TaskController };
